cf6aa72ddcebf21228940fdcd2e624d2cfc639b7
Trym Lintzen <Trymlintzen@Macintosh-van-Trym.local>
2017-11-01 11:15:58 +0100

    update 1

---
+++ DataLayer/DiveMapService.swift
@@ -0,0 +1,17 @@
+//
+//  DiveMapService.swift
+//  diveMap
+//
+//  Created by Trym Lintzen on 01-11-17.
+//  Copyright © 2017 Trym. All rights reserved.
+//
+
+import Foundation
+
+class HackerNewsService {
+    public static let sharedInstance = HackerNewsService()  // Singleton: https://en.wikipedia.org/wiki/Singleton_pattern
+    
+    private init() { // Singleton: https://en.wikipedia.org/wiki/Singleton_pattern
+    }
+    
+}
